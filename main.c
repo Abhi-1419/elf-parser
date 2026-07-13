e2b6fc78a4fb292a7db8b6609cf1fc4a912a668e
Abhi-1419 <absr0714@gmail.com>
2026-07-14 00:03:07 +0530

    Read ELF header bytes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(int argc, char *argv[])
 {
 if (argc < 2)
@@ -10,6 +10,7 @@ if (argc < 2)
 }
    for (int i = 1; i < argc; i++){
 FILE *fp = fopen(argv[i], "r");
+unsigned char header[16];
 
 if (fp == NULL)
 {
@@ -17,12 +18,15 @@ if (fp == NULL)
 continue;
 
 }
+fread(header, 1, 16, fp);
+printf("File: %s\n", argv[i]);
+for (int j = 0; j < 16; j++){
+printf("%02X ", header[j]); 
 
-printf("Opened %s successfully!\n", argv[i]);
-fclose(fp); 
+}
+printf("\n");
+fclose(fp);  
 
 }
     return 0;
-
-
 }
