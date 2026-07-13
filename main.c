6638ab84f42baaf8246af3aa9f3f5085263e8fdd
Abhi-1419 <absr0714@gmail.com>
2026-07-13 23:14:14 +0530

    Implement command-line argument and file handling

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+if (argc < 2)
+{
+    printf("Usage: %s <elf_file> [elf_file...]\n", argv[0]);
+    return 1;
+}
+   for (int i = 1; i < argc; i++){
+FILE *fp = fopen(argv[i], "r");
+
+if (fp == NULL)
+{
+    printf("Could not open %s\n", argv[i]);
+continue;
+
+}
+
+printf("Opened %s successfully!\n", argv[i]);
+fclose(fp); 
+
+}
+    return 0;
+
+
+}
