b545ca0b2492f336af38d29db5aa6f7c2e56418d
Abhi-1419 <absr0714@gmail.com>
2026-07-15 00:39:06 +0530

    Parse ELF header fields and introduce ELF header struct

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
-
+#include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
 #include <stdlib.h>
+struct ELF_Header
+{
+   uint8_t e_ident[16];
+uint16_t e_type;
+uint16_t e_machine;
+uint32_t e_version;
+uint64_t e_entry;
+uint64_t e_phoff;
+uint64_t e_shoff;
+};
+
 int main(int argc, char *argv[])
+
 {
 if (argc < 2)
 {
@@ -10,8 +23,7 @@ if (argc < 2)
 }
    for (int i = 1; i < argc; i++){
 FILE *fp = fopen(argv[i], "r");
-unsigned char header[16];
-
+struct ELF_Header elf;
 if (fp == NULL)
 {
     perror(argv[i]);
@@ -20,21 +32,21 @@ continue;
 }
 
 
-size_t bytes_read=fread(header, 1, 16, fp);
+size_t bytes_read = fread(&elf, sizeof(elf), 1, fp);
 
-if(bytes_read != 16){
+if(bytes_read != 1){
 printf("File: %s\n", argv[i]);
 printf("Could not read ELF header\n");
 fclose(fp);
 continue;}
 
-if(header[0] == 0x7F && 
+if(elf.e_ident[0] == 0x7F && 
 
-header[1] == 'E' && 
+elf.e_ident[1] == 'E' && 
 
-header[2] == 'L' && 
+elf.e_ident[2] == 'L' && 
 
-header[3] == 'F' ){ 
+elf.e_ident[3] == 'F' ){ 
 printf("File: %s\n", argv[i]);
 printf("✓ Valid ELF file\n");}
 else{
@@ -43,7 +55,7 @@ printf("File: %s\n", argv[i]);
 fclose(fp); 
  continue;}
 
-switch (header[4])
+switch (elf.e_ident[4])
 {
     case 1:
         printf("Class: ELF32\n");
@@ -60,7 +72,7 @@ switch (header[4])
 
 
 
-switch (header[5])
+switch (elf.e_ident[5])
 {
     case 1:
         printf("Endian: Little Endian\n");
@@ -74,7 +86,7 @@ switch (header[5])
         printf("Endian: Unknown\n");
         break;
 }
-switch (header[6])
+switch (elf.e_ident[6])
 {
     case 0:
         printf("Version: Invalid\n");
@@ -85,11 +97,11 @@ switch (header[6])
         break;
 
     default:
-        printf("Version: Unknown (%u)\n", header[6]);
+        printf("Version: Unknown (%u)\n", elf.e_ident[6]);
         break;
 }
 
-switch (header[7])
+switch (elf.e_ident[7])
 {
     case 0:
         printf("OS ABI: System V\n");
@@ -100,14 +112,62 @@ switch (header[7])
         break;
 
     default:
-        printf("OS ABI: Unknown (%u)\n", header[7]);
+        printf("OS ABI: Unknown (%u)\n", elf.e_ident[7]);
         break;
 }
 
-for (int j = 0; j < 16; j++){
-printf("%02X ", header[j]); 
+switch (elf.e_type)
+{
+    case 1:
+        printf("Type: Relocatable object (.o)\n");
+        break;
 
+    case 2:
+        printf("Type: Executable\n");
+        break;
+
+    case 3:
+         printf("Type: ET_DYN (Shared object or PIE executable)\n");
+        break;
+
+    case 4:
+        printf("Type: Core dump\n");
+        break;
+
+    default:
+        printf("Type: Unknown\n");
+        break;
 }
+
+switch (elf.e_machine)
+{
+    case 3:
+        printf("Machine: x86\n");
+        break;
+
+    case 40:
+        printf("Machine: ARM\n");
+        break;
+
+    case 62:
+        printf("Machine: AMD x86-64\n");
+        break;
+
+    case 183:
+        printf("Machine: AArch64\n");
+        break;
+
+    case 243:
+        printf("Machine: RISC-V\n");
+        break;
+
+    default:
+        printf("Machine: Unknown (%u)\n", elf.e_machine);
+        break;
+}
+printf("Entry point: 0x%lX\n", elf.e_entry);
+printf("Program Header Offset: %lu bytes\n", elf.e_phoff);
+printf("Section Header Offset: %lu bytes\n", elf.e_shoff);
 printf("\n");
 fclose(fp);  
 
