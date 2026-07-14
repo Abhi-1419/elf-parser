35431b38b63052dd7c2b149c8cfe18e227e32208
Abhi-1419 <absr0714@gmail.com>
2026-07-14 22:40:09 +0530

    Decode ELF identification fields

--- main.c
+++ main.c
@@ -37,13 +37,72 @@ header[2] == 'L' &&
 header[3] == 'F' ){ 
 printf("File: %s\n", argv[i]);
 printf("✓ Valid ELF file\n");}
-
 else{
 printf("File: %s\n", argv[i]);
  printf("✗ Not an ELF file\n");
-
 fclose(fp); 
  continue;}
+
+switch (header[4])
+{
+    case 1:
+        printf("Class: ELF32\n");
+        break;
+
+    case 2:
+        printf("Class: ELF64\n");
+        break;
+
+    default:
+        printf("Class: Unknown\n");
+        break;
+}
+
+
+
+switch (header[5])
+{
+    case 1:
+        printf("Endian: Little Endian\n");
+        break;
+
+    case 2:
+        printf("Endian: Big Endian\n");
+        break;
+
+    default:
+        printf("Endian: Unknown\n");
+        break;
+}
+switch (header[6])
+{
+    case 0:
+        printf("Version: Invalid\n");
+        break;
+
+    case 1:
+        printf("Version: Current\n");
+        break;
+
+    default:
+        printf("Version: Unknown (%u)\n", header[6]);
+        break;
+}
+
+switch (header[7])
+{
+    case 0:
+        printf("OS ABI: System V\n");
+        break;
+
+    case 3:
+        printf("OS ABI: Linux\n");
+        break;
+
+    default:
+        printf("OS ABI: Unknown (%u)\n", header[7]);
+        break;
+}
 
 for (int j = 0; j < 16; j++){
 printf("%02X ", header[j]); 
